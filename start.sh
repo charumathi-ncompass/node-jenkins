2155b4305e0edcea81cf95ca27485a975f724935
charumathi-ncompass <110650824+charumathi-ncompass@users.noreply.github.com>
2023-06-08 19:25:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y aws-cli docker git
-sudo yum install ruby
-sudo yum install nodejs
-sudo yum install npm
-sudo yum install java
-sudo yum install maven
-cd /home/ec2-user
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
+

67ff8f94478e389db4837ed1f4af698997b79a13
charumathi-ncompass <110650824+charumathi-ncompass@users.noreply.github.com>
2023-06-08 23:48:09 +0530

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-# stoping apache
+# stoping 
+
+# Find the process ID (PID) of your Node.js application
+PID=$(pgrep -f "node ./index.js")
+
+# Check if the application process is running
+if [ -z "$PID" ]; then
+  echo "Application is not running."
+else
+  # Terminate the application process
+  kill $PID
+  echo "Application stopped."
+fi
+
 rm -r hello.txt

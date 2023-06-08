5b85a5d2ae1473e8a5e23532be52cb59cf9f7c9f
charumathi-ncompass <110650824+charumathi-ncompass@users.noreply.github.com>
2023-06-08 19:03:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ sudo yum install -y aws-cli docker git
 sudo yum install ruby
 sudo yum install nodejs
 sudo yum install npm
+sudo yum install java
+sudo yum install maven
 cd /home/ec2-user
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install

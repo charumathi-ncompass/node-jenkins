21944f6567e4235cf112e143bcfe87b39dd211a9
charumathi-ncompass <110650824+charumathi-ncompass@users.noreply.github.com>
2023-06-08 17:41:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install -y aws-cli docker git
-
 sudo yum install ruby
 sudo yum install nodejs
 sudo yum install npm
-
 cd /home/ec2-user
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
